cd4ce96ca38c60b83a31d8719faa0980ed1ee11b
hesenbg <hasn50875@gmail.com>
2025-12-01 23:05:52 +0400

    deleted uselees junk and made a playerdata class

---
+++ Assets/Player/Scripts/PlayerData.cs
@@ -0,0 +1,66 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class PlayerData
+{
+    public static PlayerMovement movement;
+    public static AnimationLogic animationLogic;
+    public static ShootLogic shootLogic;
+    public static HealthManager healthManager;
+    public static GroundCheck groundCheck;
+
+    // Movement
+    public static void SetMovement(PlayerMovement value)
+    {
+        movement = value;
+    }
+
+    public static PlayerMovement GetMovement()
+    {
+        return movement;
+    }
+
+    // AnimationLogic
+    public static void SetAnimationLogic(AnimationLogic value)
+    {
+        animationLogic = value;
+    }
+
+    public static AnimationLogic GetAnimationLogic()
+    {
+        return animationLogic;
+    }
+
+    // ShootLogic
+    public static void SetShootLogic(ShootLogic value)
+    {
+        shootLogic = value;
+    }
+
+    public static ShootLogic GetShootLogic()
+    {
+        return shootLogic;
+    }
+
+    // HealthManager
+    public static void SetHealthManager(HealthManager value)
+    {
+        healthManager = value;
+    }
+
+    public static HealthManager GetHealthManager()
+    {
+        return healthManager;
+    }
+
+    // GroundCheck
+    public static void SetGroundCheck(GroundCheck value)
+    {
+        groundCheck = value;
+    }
+
+    public static GroundCheck GetGroundCheck()
+    {
+        return groundCheck;
+    }
+}
---
+++ Assets/Player/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    // --- Movement Parameters ---
+    [SerializeField] float JumpForce;
+    [SerializeField] public float MaxVelocityGround;
+    [SerializeField] float MaxVelocityAir;
+    [SerializeField] float GroundAcc;
+    [SerializeField] float AirAcc;
+    [SerializeField] float CurrMaxVelocity;
+    [SerializeField] float CurrAcc;
+    [SerializeField] public Vector3 Velocity;
+
+    // --- State ---
+    public bool IsGround = false;
+    public bool IsMoving = false;
+
+    // --- References ---
+    Rigidbody rb;
+    [SerializeField] AnimationLogic AnimationLogic;
+    [SerializeField] BoxCollider PlayerHitbox;
+    [SerializeField] GroundCheck GroundCheck;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        PlayerHitbox = GetComponent<BoxCollider>();
+
+        BaseHitBoxSize = PlayerHitbox.size;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    [SerializeField] float CurrentVelocity = 0;
+    void HandleHorizontalMovement()
+    {
+        Velocity = rb.linearVelocity;
+        if (Input.GetKey(KeyCode.W))
+        {
+            Velocity += (transform.forward * CurrAcc * Time.deltaTime) ;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Velocity -= (transform.forward * CurrAcc * Time.deltaTime) ;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Velocity += (transform.right * CurrAcc * Time.deltaTime) ;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Velocity -= (transform.right * CurrAcc * Time.deltaTime) ;
+        }
+        Clamp();
+    }
+    Vector3 BaseHitBoxSize;
+    public bool IsCrouching= false;
+    [SerializeField] float CrouchSpeed = 5f; 
+    Vector3 TargetHitBoxSize;
+
+    void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            TargetHitBoxSize = new Vector3(0.7f, 1, 0.7f);
+            IsCrouching = true;
+        }
+        else
+        {
+            TargetHitBoxSize = BaseHitBoxSize;
+            IsCrouching = false;
+        }
+        PlayerHitbox.size = Vector3.Lerp(PlayerHitbox.size, TargetHitBoxSize, Time.deltaTime * CrouchSpeed);
+    }
+    void Clamp()
+    {
+        // clamp horizontal speed to CurrMaxVelocity
+        Vector3 horizontal = new Vector3(Velocity.x, 0, Velocity.z);
+        if (horizontal.magnitude > CurrMaxVelocity && CurrMaxVelocity!=0)
+        {
+            horizontal = horizontal.normalized * CurrMaxVelocity;
+            Velocity.x = horizontal.x;
+            Velocity.z = horizontal.z;
+        }
+    }
+    void SetCurrAcc()
+    {
+        if (IsGround)
+        {
+            CurrAcc = GroundAcc;
+        }
+        else
+        {
+            CurrAcc = AirAcc;
+        }
+    }
+    void SetVelocityBorders()
+    {
+        if (IsGround)
+        {
+            CurrMaxVelocity = MaxVelocityGround;
+            if (IsCrouching)
+            {
+                CurrMaxVelocity = MaxVelocityGround / 2;
+            }
+        }
+        else
+        {
+            CurrMaxVelocity = MaxVelocityAir;
+        }
+    }
+
+    void UpdateMovementParameters()
+    {
+        //SetCurrentVelocity();
+        SetCurrAcc();
+        SetVelocityBorders();
+        Crouch();
+    }
+    void HandleVerticalMovement()
+    {
+        if (Input.GetKey(KeyCode.Space) && IsGround)
+        {
+            Velocity.y = JumpForce;
+        }
+    }
+    public bool IsRunning;
+    void SetSituations()
+    {
+        // is running
+        if ((Velocity.z >0.5f || Velocity.z<-0.5f) || (Velocity.x >0.5f || Velocity.x<-0.5f))
+        {
+            IsMoving = true;
+        }
+        else
+        {
+            IsMoving= false;
+        }
+        if ((Velocity.z > 2f || Velocity.z < -2f) || (Velocity.x > 2f || Velocity.x < -2f))
+        {
+            IsRunning = true;
+        }
+        else
+        {
+            IsRunning = false;
+        }
+
+        if (IsRunning && !IsCrouching)
+        {
+           //SoundManager.Instance.PlayPlayerFootSteps(transform.position);
+        }
+
+        // is pressing ground
+    }
+    void ApplyVelocity()
+    {
+        rb.linearVelocity = Velocity;
+        
+        //Velocity = Vector3.zero;
+    }
+
+    void HandleMovement() {
+        HandleHorizontalMovement();
+        HandleVerticalMovement();
+        ApplyVelocity();
+    }
+
+    private void Update()
+    {
+        UpdateMovementParameters();
+        HandleMovement();
+        SetSituations();
+    }
+
+    private void OnDrawGizmos()
+    {
+
+    }
+
+}
